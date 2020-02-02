943c0ef8a06a9407f530776dae419b37be713b34
*addsisem* <sisemorea@berea.edu>
2020-02-01 23:50:42 -0500

    Completed Second Part of the Project and updated UI

--- app/src/main/java/com/example/flixster/Models/Movie.java
+++ app/src/main/java/com/example/flixster/Models/Movie.java
@@ -3,21 +3,33 @@ package com.example.flixster.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Movie {
 
+    int movieID;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by Parcel library
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
 
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -37,6 +49,11 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
+    public String getBackdropPath() {
+
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
     public String getTitle() {
 
         return title;
@@ -45,5 +62,15 @@ public class Movie {
     public String getOverview() {
 
         return overview;
+    }
+
+    public double getRating() {
+
+        return rating;
+    }
+
+    public int getMovieID() {
+
+        return movieID;
     }
 }
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,19 +1,26 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.Models.Movie;
 import com.example.flixster.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -62,6 +69,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView tvPoster;
@@ -73,13 +81,40 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             tvPoster = itemView.findViewById(R.id.tvPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
 
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(tvPoster);
+            String imageURL;
+
+            // If phone is in landscape mode
+            if (context.getResources(). getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+                // Then imageURL = back drop image
+                imageURL = movie.getBackdropPath();
+
+            } else {
+
+                // Else imageURL = poster image
+                imageURL = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageURL).into(tvPoster);
+
+            // 1. Register click Listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
